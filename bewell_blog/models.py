65f4b926492dbd614b75157460824691d352d904
Magda Dmitruk <dmitrukm@gmail.com>
2024-03-12 19:08:13 +0000

    fixing a bug

--- bewell_blog/models.py
+++ bewell_blog/models.py
@@ -20,10 +20,10 @@ class Post(models.Model):
 # Comment Model
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
