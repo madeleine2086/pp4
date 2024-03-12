d0f33e1230a633936185f36bc6236eabc66cfaf1
Magda Dmitruk <dmitrukm@gmail.com>
2024-03-12 15:18:58 +0000

    secure secret key, ready for deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mv1vb(=_q)5@_fu(s3n-1vehunq3^)+l^-d8ajnp8kicw0g=97'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
