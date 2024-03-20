3294bb9a012344d758a2ed05963de492595e5b3b
Magda Dmitruk <dmitrukm@gmail.com>
2024-03-20 17:06:31 +0000

    add post_detail view and images folder to static

--- bewell_blog/views.py
+++ bewell_blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -8,3 +8,19 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "bewell_blog/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+   Single post detail view
+
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "bewell_blog/post_detail.html",
+        {"post": post},
+    )
