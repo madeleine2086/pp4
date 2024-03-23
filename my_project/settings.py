06084aff309d4e1ac85e13db72f11652804f89f2
Magda Dmitruk <dmitrukm@gmail.com>
2024-03-23 21:21:59 +0000

    added new styles to header and footer

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-madeleine2086-pp4-8cp6mc5i17p.ws-eu110.gitpod.io','.herokuapp.com']
 
